5791e8c20259996b753d7563ef70de411c40b83d
AMadmanWithABox <carter.gamble014@gmail.com>
2022-04-07 18:42:18 -0600

    initial

--- Bobot.py
+++ Bobot.py
@@ -6,9 +6,16 @@ if (cap.isOpened() == False):
     print("Error opening video file")
 
 while(cap.isOpened()):
-    ret, frame = cap.read()
+    ret, frame1 = cap.read()
+    ret, frame2 = cap.read()
+
+    dif = cv2.absdiff(frame1, frame2)
+    dif_gray = cv2.cvtColor(dif, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(dif_gray, (5,5), 0)
+    _, 
+
     if ret == True:
-        cv2.imshow('Frame', frame)
+        cv2.imshow('Frame', blur)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
     else:
