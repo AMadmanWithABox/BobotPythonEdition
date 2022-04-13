5130220ee176324150be1573c7a84745605e9c76
MatthewM5293 <mmunozramirez@student.neumont.edu>
2022-04-13 16:24:45 -0600

    Added SSH 1

--- DirectionSend.py
+++ DirectionSend.py
@@ -1,8 +1,25 @@
+import paramiko
+
+def connect():
+    command = "python3 Directions/Forward.py"
+
+    host = "192.168.81.8.211"
+    port = 22
+    username = "pi"
+    password = "BeepBoop"
+
+    client = paramiko.client.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(host, port, username, password)
+    #_stdin, _stdout,_stderr = client.exec_command("python3 Directions/Forward.py")
+    #print(stdout.read().decode())
+    client.close()
 
 def forward():
-
+    print("Fowrward")
 def left():
-
+    print("left")
 def right():
-
+    print("right")
 def reverse():
+    print("reverse")
