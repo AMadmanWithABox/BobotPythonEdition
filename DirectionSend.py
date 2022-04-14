88ea5bdf4569db1229ba8ef1e6fcc3897e1e8bb5
AMadmanWithABox <carter.gamble014@gmail.com>
2022-04-13 18:24:56 -0600

    Gave directions functions in DirectionSend.py the ability to move motors and moved client.close to Window.py 's exit() function

--- DirectionSend.py
+++ DirectionSend.py
@@ -3,23 +3,26 @@ import paramiko
 def connect():
     command = "python3 Directions/Forward.py"
 
-    host = "192.168.81.8.211"
+    host = "192.168.8.211"
     port = 22
     username = "pi"
     password = "BeepBoop"
 
+    global client
     client = paramiko.client.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(host, port, username, password)
-    client.exec_command("python3 Directions/Forward.py")
     #print(stdout.read().decode())
-    client.close()
 
 def forward():
     print("Fowrward")
+    client.exec_command("python3 Directions/Forward.py")
 def left():
     print("left")
+    client.exec_command("python3 Directions/Left.py")
 def right():
     print("right")
+    client.exec_command("python3 Directions/Right.py")
 def reverse():
     print("reverse")
+    client.exec_command("python3 Directions/Reverse.py")
--- Window.py
+++ Window.py
@@ -1,19 +1,28 @@
 from tkinter import *
-import threading, MotionDetection
+import threading, MotionDetection, DirectionSend
 
 root = Tk()
 t1 = threading.Thread(target = MotionDetection.start_obj_detect)
 
+DirectionSend.connect()
+
 root.geometry('150x200')
 
-btn1 = Button(root, text = 'Start object detection', bd = '5', command = t1.start())
-btn2 = Button(root, text = 'Start motion following', bd = '5', command = root.destroy)
-btn3 = Button(root, text = 'Start controller', bd = '5', command = root.destroy)
-btn4 = Button(root, text = 'Exit', bd = '5', command = root.destroy)
+btn1 = Button(root, text = 'Start object detection', bd = '5', command = t1.start)
+btn2 = Button(root, text = 'Start motion following', bd = '5', command = exit)
+btn3 = Button(root, text = 'Start controller', bd = '5', command = exit)
+btn4 = Button(root, text = 'Exit', bd = '5', command = exit)
 
 btn1.pack(side = 'top')
 btn2.pack(side = 'top')
 btn3.pack(side = 'top')
 btn4.pack(side = 'top')
 
-root.mainloop()
+try:
+    root.mainloop()
+except:
+    print("root did not main loop")
+
+def exit():
+    DirectionSend.client.close()
+    root.destroy()
